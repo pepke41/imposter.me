104fa892f22aa044666a9e4de3e5bdd872534089
Annie <anne.latsko@gmail.com>
2016-01-30 13:41:12 -0500

    fixed root path and deleted /imposter/ route

--- imposter.me/db/schema.rb
+++ imposter.me/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130045130) do
+ActiveRecord::Schema.define(version: 20160130165810) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
