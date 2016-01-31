cfbe53973758a6c223482b42bc8a47e5dac0f6d0
Annie <anne.latsko@gmail.com>
2016-01-30 19:59:15 -0500

    added the name of the author underneath the posts and implemented light tag search functionality

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,10 @@ ActiveRecord::Schema.define(version: 20160130210250) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.string   "post_author_name"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "resources", force: :cascade do |t|
